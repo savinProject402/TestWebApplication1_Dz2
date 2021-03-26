3cd66519c31a80812bb12a21058d6b56a4cebf5b
savinProject402 <74914774+savinProject402@users.noreply.github.com>
2021-03-26 06:03:38 +0200

    Add files via upload

---
+++ Smartphones.Domain/Contracs/ISmartphonesServices.cs
@@ -0,0 +1,15 @@
+﻿using Smartphones.Domain.Model;
+using Smartphones.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smartphones.Domain.Contracs
+{
+   public interface ISmartphonesServices
+    {
+          IReadOnlyCollection<SmartphonesModel> GetAll();
+    }
+}
---
+++ Smartphones.Domain/Model/ShopNetworkModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smartphones.Domain.Model
+{
+   public class ShopNetworkModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string OwnerId { get; set; }
+
+        public ICollection<ShopModel> Shops { get; set; }
+    }
+}
